17e7d269d71f9a56a8340b3a2000c959339788bd
Maxime Daniel <root@maxux.net>
2018-05-20 16:34:54 +0200

    Move structs to header file

--- lantraffic.c
+++ lantraffic.c
@@ -15,45 +15,7 @@
 #include <string.h>
 #include <netdb.h>
 #include <hiredis/hiredis.h>
-
-#define SNAPSHOTLEN    1514
-#define PROMISCMODE    0
-#define BUFFERTIME     100
-
-typedef struct run_t {
-    uint64_t rx;
-    uint64_t tx;
-
-} run_t;
-
-typedef struct client_t {
-    run_t traffic;
-    run_t lifetime;
-    uint32_t rawip;
-    char address[16];
-    char *hostname;
-
-} client_t;
-
-typedef struct clients_t {
-    client_t *list;
-    uint64_t length;
-
-} clients_t;
-
-typedef struct userdata_t {
-    uint32_t localnet;
-    uint32_t localmask;
-    uint64_t run;
-
-    time_t dumptime;
-
-    run_t lifetime;
-    run_t runtotal;
-
-    clients_t clients;
-
-} userdata_t;
+#include "lantraffic.h"
 
 int prevCheck = 0;
 
